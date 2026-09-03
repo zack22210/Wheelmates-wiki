78583c142c0c31d6758909c1697c56808d9913d4
Zackol <149146549+zack22210@users.noreply.github.com>
2026-09-03 21:52:05 +0800

    render locale routes on demand

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -3,6 +3,8 @@ import {getLegalCopy, getLegalMetadata} from '@/lib/legal';
 
 type Props = {params: Promise<{locale: string}>};
 
+export const dynamic = 'force-dynamic';
+
 export async function generateMetadata({params}: Props) {
   const {locale} = await params;
   return getLegalMetadata('about', '/about', locale);
@@ -12,3 +14,4 @@ export default async function AboutPage({params}: Props) {
   const {locale} = await params;
   return <LegalPage {...await getLegalCopy('about', locale)} />;
 }
+
